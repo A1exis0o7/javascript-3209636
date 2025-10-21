5a0d2e9c861a560f82512301950ad7957cdb85d0
A1exis0o7 <alexiselpro2019@gmail.com>
2025-10-21 15:26:29 -0500

    Add files via upload

--- 1-ecenario-interactivo/js.js
+++ 1-ecenario-interactivo/js.js
@@ -1,1 +1,26 @@
 
+const monedas = document.querySelectorAll('.moneda');
+const contador = document.querySelectorAll('.conteo');
+
+
+
+
+let i = 0;
+
+console.log(monedas);
+console.log(contador);
+
+monedas.forEach(item => {
+    
+    item.addEventListener('click', () => {
+        item.classList.add('saltar');
+
+        i++
+        contador.textContent = `contador: ${i}`;
+
+} )
+
+    item.addEventListener('animationend', () => {
+        item.computedStyleMap.display = 'none';
+    }, {once: true} )
+})
