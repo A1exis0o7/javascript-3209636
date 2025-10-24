8e9fc41db7339ce2b5ee4ec6a240a3973090665c
A1exis0o7 <alexiselpro2019@gmail.com>
2025-10-24 15:40:19 -0500

    Add files via upload

--- 1-ecenario-interactivo/js.js
+++ 1-ecenario-interactivo/js.js
@@ -1,26 +1,60 @@
 
-const monedas = document.querySelectorAll('.moneda');
-const contador = document.querySelectorAll('.conteo');
+const monedas = document.querySelectorAll(".moneda");
+const contador = document.querySelector(".conteo");
+let numero = 0;
 
 
 
 
-let i = 0;
 
-console.log(monedas);
-console.log(contador);
+console.log(monedas)
+console.log(contador)
 
 monedas.forEach(item => {
     
-    item.addEventListener('click', () => {
-        item.classList.add('saltar');
+    item.addEventListener("click", () => {
+        item.classList.add("saltar");
 
-        i++
-        contador.textContent = `contador: ${i}`;
+        numero++
+        if (contador) {
+            contador.textContent = numero;
+        }
 
 } )
 
-    item.addEventListener('animationend', () => {
-        item.computedStyleMap.display = 'none';
+    item.addEventListener("animationend", () => {
+        item.style.display = "none"
     }, {once: true} )
 })
+
+//**juego 2//**
+
+
+const monedas2 = document.querySelectorAll(".huevo");
+const contador2 = document.querySelector(".conteo2");
+let numero2 = 0;
+
+
+
+
+
+console.log(monedas2)
+console.log(contador2)
+
+monedas2.forEach(item => {
+    
+    item.addEventListener("click", () => {
+        item.classList.add("saltar");
+
+        numero++
+        if (contador2) {
+            contador2.textContent = numero;
+        }
+
+} )
+
+    item.addEventListener("animationend", () => {
+        item.style.display = "none"
+    }, {once: true} )
+})
+
