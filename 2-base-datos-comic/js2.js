45fb36ea3099bbc6fe8ecbb9beb83aa0f779f811
A1exis0o7 <alexiselpro2019@gmail.com>
2025-11-06 15:21:04 -0500

    base de datos comic

---
+++ 2-base-datos-comic/js2.js
@@ -0,0 +1,64 @@
+let comic = {
+  "nombrecomic": "Cenizas de honor",
+  "sinopsis": "un caballero se",
+  "year": 2025,
+  "autores": ["Alexis"," miguel", "isabel", "luna",],
+  "personajes":["ninove","elvira","bruja", "leon"],
+  "portada": "./sdbvjsb",
+  "genero": "ficcion",
+  "ecenas":
+   [
+    {
+      "ecena": "reino",
+      "id": 29,
+      "imagen": "./imagenes/sjdnvsj",
+      "personajes": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+    },
+
+    {
+      "name": "Molecule Man",
+      "id": 29,
+      "imagen": "./imagenes/sjdnvsj",
+      "personajes": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+    },
+
+    {
+      "name": "Molecule Man",
+      "id": 29,
+      "imagen": "./imagenes/sjdnvsj",
+      "personajes": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+    },
+
+
+
+
+
+
+
+
+
+
+    {
+      "name": "Madame Uppercut",
+      "age": 39,
+      "secretIdentity": "Jane Wilson",
+      "powers": [
+        "Million tonne punch",
+        "Damage resistance",
+        "Superhuman reflexes"
+      ]
+    },
+    {
+      "name": "Eternal Flame",
+      "age": 1000000,
+      "secretIdentity": "Unknown",
+      "powers": [
+        "Immortality",
+        "Heat Immunity",
+        "Inferno",
+        "Teleportation",
+        "Interdimensional travel"
+      ]
+    }
+  ]
+}
